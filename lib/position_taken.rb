84133ddfa9b5c52477345d5f41c8101a9dcfca3b
Harish Manoharan <harishmanoharan17@gmail.com>
2020-03-30 09:26:48 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,10 +1,10 @@
 # code your #position_taken? method here!
 def position_taken?(board,index)
   if board[index]==" "
-    return true
-  elsif board[index]==""
-    return true
-  else
     return false
+  elsif board[index]==""
+    return false
+  else
+    return true
   end
 end
